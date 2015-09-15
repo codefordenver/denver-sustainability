94e80c9bf4babb64923f3fadcc47d7b9d0c20f38
Mike Davlantes <rocks722@hotmail.com>
2015-09-14 20:51:00 -0600

    Added filterLegend function; updated legend icons

--- js/initLegend.js
+++ js/initLegend.js
@@ -2,7 +2,7 @@ function initLegend(map) {
     // List of legend labels.
     labels = new Array('Energy Star Buildings', 'Large buildings', 'Small Buildings');
     // List of legend icons
-    icons = new Array('images/energy_star_logo_small.png', 'images/measle_blue.png', 'images/green_circle_icon_md_20px.png');
+    icons = new Array('images/energy_star_logo_small.png', 'images/icon-circle-greenx20.png', 'images/icon-circle-grayx20.png');
     var legend = document.getElementById('legend');
     for (i = 0; i < labels.length; i++) {
       var div = document.createElement('div');
@@ -15,4 +15,23 @@ function initLegend(map) {
     }
 
     map.controls[google.maps.ControlPosition.TOP_RIGHT].push(legend);
+}
+
+function filterLegend(buildingColor, map){
+  // set buildingColor to '' to restore all markers
+  var state = null;
+  var markersArray = [];
+  if (buildingColor=='green') {
+    markersArray = grayBuildings.concat(energyStarBuildings);
+  } else if (buildingColor=='gray'){
+    markersArray = greenBuildings.concat(energyStarBuildings);
+  } else if (buildingColor=='blue'){
+    markersArray = grayBuildings.concat(greenBuildings);
+  } else {
+    markersArray.concat(greenBuildings).concat(energyStarBuildings).concat(grayBuildings);
+    state = map;
+  }
+  for (var i = 0; i < markersArray.length; i++) {
+    markersArray[i].setMap(state);
+  }
 }
